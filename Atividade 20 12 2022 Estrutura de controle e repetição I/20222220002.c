aac42e092dcf8fe8ce21969f9dd927fa85e93534
Mateus Bastos <120401672+mateusb261uea@users.noreply.github.com>
2022-12-21 21:44:11 -0400

    Add files via upload

---
+++ Atividade 20 12 2022 Estrutura de controle e repetição I/20222220002.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int contador;
+    int soma = 0;
+    int temp = 0;
+
+    for (contador=0; contador<50; contador++){
+        printf("digite um numero: ");
+        scanf("%d", & temp);
+
+        if (temp % 2 != 0){
+            soma+=temp;}}
+    printf("%d", soma);
+    return 0;}
