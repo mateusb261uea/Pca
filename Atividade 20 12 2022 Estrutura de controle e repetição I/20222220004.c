1a2101b58a3e436439b3b82ddcab5ee852a9cf37
Mateus Bastos <120401672+mateusb261uea@users.noreply.github.com>
2022-12-21 22:21:36 -0400

    Add files via upload

---
+++ Atividade 20 12 2022 Estrutura de controle e repetição I/20222220004.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+    int contador;
+    int temp = 0;
+    int num_maior = 0;
+    int num_menor = 0;
+
+    for (contador=0; contador<4; contador++){
+
+        printf("digite um numero: ");
+        scanf("%d", & temp);
+
+        if (contador == 0){
+
+            num_maior = temp;
+            num_menor = temp;
+        }
+
+        else{
+
+            if(temp < num_menor){
+                num_menor = temp;
+            }
+            else{
+                if(temp > num_maior){
+                    num_maior = temp;
+                }
+            }
+        }
+    }
+    printf("%d foi o maior valor fornecido \n", num_maior);
+    printf("%d foi o menor valor fornecido", num_menor);
+    return 0;
+}
